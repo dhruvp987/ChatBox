72ddf0413608cab7367d13ae2a5ba999725917ea
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-06-04 13:43:09 -0400

    Use FastAPI to provide a web API for chat

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
+import os
 import sys
+from fastapi import FastAPI
+from pydantic import BaseModel
 from llamacppllm import LlamaCppLlm
 
 # A context window that doesn't take too much resources, making
 # it easier to work with for now
 MAX_CTX = 2048
 
-EXIT_STR = '/exit'
 
-MAX_CMD_ARGS = 2
-def usage_msg():
-    return 'usage: chatbox model_path'
+def print_err(msg):
+    print(f'Error: {msg}', file=sys.stderr)
 
 
-def main():
-    if len(sys.argv) != MAX_CMD_ARGS:
-        print(usage_msg(), file=sys.stderr)
-        sys.exit(1)
-
-    llm = LlamaCppLlm(sys.argv[1], MAX_CTX)
-
-    while True:
-        prompt = input(f'Enter your prompt ({EXIT_STR} to exit): ')
-        if prompt == EXIT_STR:
-            break
-        print(llm.chat(prompt), '\n')
+class Prompt(BaseModel):
+    prompt: str
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+model_path = os.getenv('MODEL_PATH', '')
+if model_path == '':
+    print_err('MODEL_PATH env is not set')
+    sys.exit(1)
+
+# If using fastapi dev, make sure the --no-reload option is set,
+# otherwise the LLM will be loaded multiple times and use
+# excessive resources
+llm = LlamaCppLlm(model_path, MAX_CTX)
+
+
+@app.get('/')
+async def root():
+    return 'Welcome to ChatBox'
+
+
+@app.post('/chat')
+async def chat(prmt: Prompt):
+    return llm.chat(prmt.prompt)

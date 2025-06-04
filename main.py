f3a056250aa68ee56705783d1fb2ea63bbde4c2c
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-06-03 23:08:31 -0400

    Get model loaded and have basic chat completion.

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import sys
+from llama_cpp import Llama
+
+# A context window that doesn't take too much resources, making
+# it easier to work with for now
+MAX_CTX = 2048
+
+MAX_CMD_ARGS = 3
+def usage_msg():
+    return 'usage: chatbox model_path prompt'
+
+
 def main():
-    print("Hello from chatbox!")
+    if len(sys.argv) != MAX_CMD_ARGS:
+        print(usage_msg(), file=sys.stderr)
+        sys.exit(1)
+
+    llm = Llama(
+        model_path=sys.argv[1],
+        n_ctx=MAX_CTX,
+        verbose=False
+    )
+
+    print(llm.create_chat_completion(
+        messages = [
+            {'role': 'user', 'content': sys.argv[2]}
+        ]
+    )['choices'][0]['message']['content'])
 
 
 if __name__ == "__main__":

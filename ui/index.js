6761613f3de635e6451560b3131a548adcff0310
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-06-09 23:51:36 -0400

    Add Markdown rendering with Marked library
    Many LLMs format their responses with Markdown.
    This uses that format to display their responses
    nicely.

--- ui/index.js
+++ ui/index.js
@@ -1,3 +1,5 @@
+import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js';
+
 const CHAT_CONT_ID = 'chat-container';
 const PROMPT_INPUT_ID = 'prompt-input';
 const SBMT_CHAT_BTN_ID = 'submit-chat-button';
@@ -19,9 +21,9 @@ async function llmChatComp(text) {
     return json.completion;
 }
 
-function addChat(text, chatClass, chatCont) {
-    const chat = document.createElement('p');
-    chat.textContent = text;
+function addChat(mdText, chatClass, chatCont) {
+    const chat = document.createElement('div');
+    chat.innerHTML = marked.parse(mdText);
     chat.className = chatClass;
     chatCont.appendChild(chat);
 }

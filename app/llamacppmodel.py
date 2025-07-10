a33a2fb5ce00e7b4ccde47c9b58df8ae595be085
Dhruv P. <169485603+dhruvp987@users.noreply.github.com>
2025-07-10 14:31:12 -0400

    Add chunked batching

--- app/llamacppmodel.py
+++ app/llamacppmodel.py
@@ -3,6 +3,16 @@ import llama_cpp
 
 INT32_MAX = 2147483647
 
+def set_llama_cpp_batch(batch, tok_arr, n_toks, tok_start, n_past):
+    batch.n_tokens = n_toks
+    for i in range(tok_start, tok_start + n_toks):
+        batch.token[i - tok_start] = tok_arr[i]
+        batch.pos[i - tok_start] = i - tok_start + n_past
+        batch.seq_id[i - tok_start][0] = 0
+        batch.n_seq_id[i - tok_start] = 1
+        batch.logits[i - tok_start] = False
+    batch.logits[n_toks - 1] = True
+
 class LlamaCppError(Exception):
     pass
 
@@ -38,12 +48,25 @@ class LlamaCppModel:
 
 
 class LlamaCppContext:
-    def __init__(self, model, n_ctx=4096, temp=0.8, top_k=40, repeat_pen=1.1, min_p=0.05, top_p=0.95):
+    def __init__(
+        self,
+        model,
+        n_ctx=4096,
+        n_batch=512,
+        temp=0.8,
+        top_k=40,
+        repeat_pen=1.1,
+        min_p=0.05,
+        top_p=0.95
+    ):
         self._model = model
         self._n_ctx = n_ctx
+        self._n_batch = n_batch
 
         ctx_params = llama_cpp.llama_context_default_params()
         ctx_params.n_ctx = n_ctx
+        ctx_params.n_batch = n_batch
+        ctx_params.n_ubatch = n_batch
         self._ctx = llama_cpp.llama_new_context_with_model(model.model, ctx_params)
         if self._ctx is None:
             raise LlamaCppError('Could not create context')
@@ -60,6 +83,8 @@ class LlamaCppContext:
         # TODO: Replace greedy decoder with random selection of token from modified prob dist
         llama_cpp.llama_sampler_chain_add(self._smpl, llama_cpp.llama_sampler_init_greedy())
 
+        self._batch = llama_cpp.llama_batch_init(n_batch, 0, 1)
+
     def complete_chat(self, chat, chat_template):
         chat_fmtted = bytes(chat_template.render(chat), 'utf-8')
 
@@ -74,8 +99,8 @@ class LlamaCppContext:
             True,
             True
         )
-        if prmpt_n_toks > self._n_ctx:
-            raise LlamaCppError('Prompt size exceeds context size')
+        if prmpt_n_toks >= self._n_ctx:
+            raise LlamaCppError('Prompt fills context size')
 
         prmpt_tok_arr = (llama_cpp.llama_token * prmpt_n_toks)()
         res = llama_cpp.llama_tokenize(
@@ -90,13 +115,14 @@ class LlamaCppContext:
         if res < 0:
             raise LlamaCppError('Could not tokenize the prompt')
 
-        batch = llama_cpp.llama_batch_get_one(prmpt_tok_arr, prmpt_n_toks)
+        for i in range(0, prmpt_n_toks, self._n_batch):
+            n_bth_or_n_toks = min(prmpt_n_toks, self._n_batch)
+            set_llama_cpp_batch(self._batch, prmpt_tok_arr, min(prmpt_n_toks - i, n_bth_or_n_toks), i, i)
+            if llama_cpp.llama_decode(self._ctx, self._batch) < 0:
+                raise LlamaCppError('Could not decode prompt')
 
         n_toks_gen = 0
-        while prmpt_n_toks + n_toks_gen < self._n_ctx:
-            if llama_cpp.llama_decode(self._ctx, batch) < 0:
-                raise LlamaCppError('Could not decode batch of tokens')
-
+        while prmpt_n_toks + n_toks_gen + 1 < self._n_ctx:
             tok_id = llama_cpp.llama_sampler_sample(self._smpl, self._ctx, -1)
             if llama_cpp.llama_vocab_is_eog(self._vocab, tok_id):
                 break
@@ -120,9 +146,13 @@ class LlamaCppContext:
 
             ind_tok_arr = (llama_cpp.llama_token * 1)()
             ind_tok_arr[0] = tok_id
-            batch = llama_cpp.llama_batch_get_one(ind_tok_arr, 1)
+            set_llama_cpp_batch(self._batch, ind_tok_arr, 1, 0, prmpt_n_toks + n_toks_gen)
+
+            if llama_cpp.llama_decode(self._ctx, self._batch) < 0:
+                raise LlamaCppError('Could not decode generated tokens')
 
     def __del__(self):
         llama_cpp.llama_free(self._ctx)
         llama_cpp.llama_sampler_free(self._smpl)
+        llama_cpp.llama_batch_free(self._batch)
 
